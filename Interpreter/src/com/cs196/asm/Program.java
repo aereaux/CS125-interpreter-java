192f733dd4ff29a6787f4a78a857a5d22a86583a
Aidan Epstein <aidan@jmad.org>
2014-10-27 15:20:41 -0500

    Added logic to interpreter.
    --HG--
    branch : interpreter-logic

--- Interpreter/src/com/cs196/asm/Program.java
+++ Interpreter/src/com/cs196/asm/Program.java
@@ -1,5 +1,8 @@
 package com.cs196.asm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * The Program will hold the commands given to it by the debugger.
  * It will execute commands sequentially upon the step method
@@ -8,13 +11,43 @@ package com.cs196.asm;
 
 public class Program 
 {
-	public Program()
+	private int PC = 0;
+	boolean N = false;
+	boolean P = false;
+	boolean Z = false;
+	List<Integer> memory = new ArrayList<Integer>();
+	int[] registers = new int[8];
+	private Instruction[] code;
+	
+	public Program(String[] code, ArrayList<Integer> inputMemory)
 	{
+		this.code = new Instruction[code.length];
+		for(int i = 0; i < code.length; i++) {
+			this.code[i] = new Instruction(code[i], this);
+		}
+		memory = inputMemory;
 	}
 	
 	//will return false when the program is finished
 	public boolean step()
 	{
-		return false;
+		if(PC > code.length) {
+			return false;
+		}
+		code[PC].execute();
+		return true;
+	}
+
+	public void incrementPC(int i) {
+		PC += i;
+	}
+
+	public void updateCodes(int i) {
+		N = false;
+		P = false;
+		Z = false;
+		if(i < 0) N = true;
+		else if(i == 0) Z = true;
+		else if(i > 0) P = true;
 	}
 }
